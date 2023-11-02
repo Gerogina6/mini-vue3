ba45353bf2eff005d4be077bba898aaf7d1cead1
georgina6 <lunarecho530@gmail.com>
2023-11-02 18:01:49 +0800

    initial track&trigger

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -4,16 +4,40 @@ class ReactiveEffect{
         this._fn = fn
     }
     run() {
-        fn() {
-            this._fn()
-        }
+        activeEffect = this
+        this._fn()
     }
 }
 
+const targetMap = new Map()
 export function track(target, key) {
+    // target -> key -> dep
+    let depsMap = targetMap.get(target)
+    if(!depsMap) {
+        depsMap = new Map()
+        targetMap.set(target, depsMap)
+    }
 
+    let dep = depsMap.get(key)
+    if(!dep) {
+        dep = new Set()
+        depsMap.set(key, dep)
+    }
+
+    dep.add(activeEffect)
+    // let dep = new Set()
 }
 
+export function trigger(target, key) {
+    let depsMap = targetMap.get(target)
+    let dep = depsMap.get(key)
+
+    for(const effect of dep) {
+        effect.run()
+    }
+}
+
+let activeEffect
 export function effect(fn) {
     const _effect = new ReactiveEffect(fn)
     _effect.run()
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,4 +1,6 @@
-export  function reactive(raw) {
+import { track, trigger } from './effect'
+
+export function reactive(raw) {
     return new Proxy(raw, {
         get(target, key) {
             const res = Reflect.get(target, key)
@@ -9,6 +11,7 @@ export  function reactive(raw) {
         set(target, key, value) {
             const res = Reflect.set(target, key, value)
             // 触发依赖
+            trigger(target, key)
             return res
         }
     })
